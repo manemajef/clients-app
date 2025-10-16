9bd72920f4afff486abe5989da37b2f9cd34559c
manemajef <rotem.semah@gmail.com>
2025-10-16 14:16:50 +0300

    change db to sqlite for dev

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,10 @@
 from sqlmodel import create_engine, Session, SQLModel
 from app.core.config import settings
 from app.models import user, client, meeting 
-database_url = settings.DATABASE_URL
+# database_url = settings.DATABASE_URL
+database_url = 
+"sqlite:///app.db"
+
 engine = create_engine(database_url, echo = False)
 
 
