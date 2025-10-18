acf9692fd9c2466f282a44e4a06d7f5e07c9178d
manemajef <rotem.semah@gmail.com>
2025-10-19 02:17:43 +0300

    fix add client test

--- backend/app/tests/test-client-add.py
+++ backend/app/tests/test-client-add.py
@@ -1,3 +1,4 @@
+
 from repositories.user_repository import UserRepository
 from repositories.client_repository import ClientRepository, ContactRepository
 from app.services.client_service import ClientService
@@ -5,32 +6,41 @@ from app.database import engine
 from sqlmodel import Session
 from app.models.client import ContactBase, ClientAdd
 
-user = "rotem"
+
+user = "jeff"
 clients = [
     ClientAdd(
-        name = "bob"
+        name="bob"
     ),
-    ClientAdd (
-        name = "alice" ,
-        contacts = [
-            ContactBase (
-                type = "phone", contact = "+972538713139"
+    ClientAdd(
+        name="alice",
+        contacts=[
+            ContactBase(
+                type="phone", contact="+972538713139"
             ),
-            ContactBase (
-                type = "email", contact = "alice@eatmyass.com"
+            ContactBase(
+                type="email", contact="alice@eatmyass.com"
             )
         ]
     )
 ]
 
+
 def test_client_add():
     with Session(engine) as db:
+        # Create repositories
         user_repo = UserRepository(db)
         client_repo = ClientRepository(db)
-        contact_repo =ContactRepository(db)
+        contact_repo = ContactRepository(db)
+
+        # Create service
         client_service = ClientService(client_repo, contact_repo, user_repo)
+
+        # Test adding clients
         for client in clients:
-            added = client_service.add_client("jeff", client)
+            added = client_service.add_client(user, client)
             print(added)
+
+
 if __name__ == "__main__":
     test_client_add()
